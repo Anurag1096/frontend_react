54af64899d84d7b7da2f1f848f81c2408eeb7696
Anurag1096 <anuragqwerty09@outlook.com>
2024-11-06 14:25:30 +0530

    feat: signup form with axios creation compleeted

--- src/components/Signup.tsx
+++ src/components/Signup.tsx
@@ -1,96 +1,126 @@
 import React,{useState} from "react"
+import axios from "axios";
 import "./signup.css"
 type UserFields={
 	name:string;
 	gender:string;
 	userName:string;
+  password:string;
 	userImage?:HTMLImageElement;
 }
 
 export const Signup=()=>{
-
+ const [loading,setLoading]=useState<boolean>(false)
  const [userFields,setFields] =useState<UserFields>({
  	name:"",
  	gender:"",
+  password:"",
  	userName:"",
- 	userImage:null,
+ 	userImage:undefined,
  })
 
  const handleUserData=(e:any)=>{
-   console.log(e.target)
-
    const {name,value,type}=e.target
-   // We will do this code when we have checked boolean val(code not correct)
-   let newVal = type === "file" ? (e.target.files.length > 0 ? e.target.files[0] : null) : value;
-
+   const newVal = type === "file" ? (e.target.files.length > 0 ? e.target.files[0] : null) : value;
    
    setFields((prevState)=>({
    	...prevState,
    	[name]:newVal,
    })) 
-
-
  }  
 
  const submitHandle=()=>{
  	console.log(userFields)
+  // sets the loading to true
+  setLoading((prevState)=>!prevState)
+   const formData = new FormData();
+  formData.append("name",userFields.name)
+  formData.append("gender",userFields.gender)
+  formData.append("userImage",userFields.userImage)
+  formData.append("username",userFields.userName)
+  formData.append("password",userFields.password) 
+ 
+
+axios.post("http://127.0.0.1:8000/signup/",formData)
+.then((res)=>{
+  setLoading(false) 
+  console.log(res)
+
+})
+.catch((error)=>{
+  setLoading(false)
+  console.log(error)
+})
+
  }
 
-	return (
-       <div className="signup-main">
-       <input type ="text"
+ return (
+  loading ? "Loading" : (
+    <div className="signup-main">
+      <input 
+        type="text"
         name="name"
-        autocomplete="on"
         value={userFields.name}
         onChange={handleUserData}
-
-        />
-
-        <input type="radio"
+      />
+      
+      <input 
+        type="password"
+        name="password"
+        value={userFields.password}
+        onChange={handleUserData}
+      />
+      
+      <div>
+        <input 
+          type="radio"
           name="gender"
-          autocomplete="on"
           value="Male"
           onChange={handleUserData}
           checked={userFields.gender === "Male"}
-            />
-
-         <input type="radio"
-         name="gender"
-         autocomplete="on"
-         value="Female"
-         onChange={handleUserData}
-         checked={userFields.gender === "Female"} />
-
-         <input type="radio"
-         name = "gender"
-         autocomplete="on"
-         value="Other"
-         onChange={handleUserData}
-         checked={userFields.gender === "Other"}/>
-        <label >User Name
-          <input type ="text"
-          autocomplete="on"
-        name="userName"
-        value={userFields.userName}
-        onChange={handleUserData}
-
         />
-             
-         </label>
+        Male
 
-         <label>
+        <input 
+          type="radio"
+          name="gender"
+          value="Female"
+          onChange={handleUserData}
+          checked={userFields.gender === "Female"}
+        />
+        Female
 
+        <input 
+          type="radio"
+          name="gender"
+          value="Other"
+          onChange={handleUserData}
+          checked={userFields.gender === "Other"}
+        />
+        Other
+      </div>
 
-         Image
-          <input type="file"
-           name="userImage"
-           onChange={handleUserData} 
-          />
+      <label>
+        User Name
+        <input 
+          type="text"
+          name="userName"
+          value={userFields.userName}
+          onChange={handleUserData}
+        />
+      </label>
 
-         </label>
+      <label>
+        Image
+        <input 
+          type="file"
+          name="userImage"
+          onChange={handleUserData}
+        />
+      </label>
 
-         <button onClick={submitHandle}>Submit</button>
-        </div>
-         
-		)
+      <button onClick={submitHandle}>Submit</button>
+    </div>
+  )
+);
 }
